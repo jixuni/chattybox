e8137b8db2f1c2e6e82c00f53a90991b0e3b5c76
Jixu Ni <jixuni@Jixus-MBP.home>
2019-05-29 14:35:30 -0400

    added login page

--- src/login/login.js
+++ src/login/login.js
@@ -1,9 +1,68 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import styles from "./styles";
 
 class LoginComponent extends React.Component {
   render() {
-    return <div>Hello World From Login</div>;
+    const { classes } = this.props;
+    return (
+      <main className={classes.main}>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Log In!
+          </Typography>
+          <form className={classes.form} onSubmit={e => this.submitLogin(e)}>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="login-email-input">
+                Enter Your Email
+              </InputLabel>
+              <Input
+                autoComplete="email"
+                autoFocus
+                id="login=email-input"
+                onChange={e => this.userTyping("email", e)}
+              />
+            </FormControl>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="login-password-input">
+                Enter Your Password
+              </InputLabel>
+              <Input
+                type="password"
+                id="login=password-input"
+                onChange={e => this.userTyping("password", e)}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Log In
+            </Button>
+          </form>
+        </Paper>
+      </main>
+    );
   }
+  userTyping = (type, e) => {
+    console.log(type, e);
+  };
+
+  submitLogin = e => {
+    console.log("Submitting!");
+  };
 }
 
-export default LoginComponent;
+export default withStyles(styles)(LoginComponent);
