7d85b8758420ec8932a488c875c094a1c75466d1
Jixu Ni <jixuni@Jixus-MBP.home>
2019-05-29 19:27:52 -0400

    added some css boilerplate to dashboard

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,9 +1,40 @@
 import React from "react";
+import ChatListComponent from "../chatlist/chatList";
 
 class DashboardCompoent extends React.Component {
-  render() {
-    return <div>Hello World From Dashboard</div>;
+  constructor() {
+    super();
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
+    };
   }
+
+  render() {
+    return (
+      <div>
+        <div>Hello World From Dashboard</div>
+        <ChatListComponent
+          history={this.props.history}
+          newChatBtnFn={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={this.state.chats}
+          userEmail={this.state.email}
+          selectedChatIndex={this.state.selectedChat}
+        />
+      </div>
+    );
+  }
+
+  selectChat = () => {
+    console.log("Selected a chat");
+  };
+
+  newChatBtnClicked = () => {
+    console.log("New Chat button Clicked!");
+  };
 }
 
 export default DashboardCompoent;
