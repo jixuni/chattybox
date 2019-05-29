99c525a3815a634e9b99ddc23d9df58b7c7de269
unknown <jixuni@gmail.com>
2019-05-28 21:46:36 -0400

    added route and initialize firebase database

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import LoginComponent from "./login/login";
+import SignupComponent from "./signup/signup";
+import DashboardComponent from "./dashboard/dashboard";
 
 const firebase = require("firebase");
 require("firebase/firestore");
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
+firebase.initializeApp({
+  apiKey: "AIzaSyAwtiPv-6wbfVEgevniABsYDdUcQboIdIc",
+  authDomain: "chatty-box-c6eed.firebaseapp.com",
+  databaseURL: "https://chatty-box-c6eed.firebaseio.com",
+  projectId: "chatty-box-c6eed",
+  storageBucket: "chatty-box-c6eed.appspot.com",
+  messagingSenderId: "936679149448",
+  appId: "1:936679149448:web:58c79891e332e193"
+});
+
+const routing = (
+  <Router>
+    <div id="routing-container">
+      <Route path="/login" component={LoginComponent} />
+      <Route path="/signup" component={SignupComponent} />
+      <Route path="/dashboard" component={DashboardComponent} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/signup/signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SignupCompoent extends React.Component {
+  render() {
+    return <div>Hello World From signup</div>;
+  }
+}
+
+export default SignupCompoent;
