036d8f51ce7b9b5d7c84894cd721991a85196b46
jixuni <jixuni@gmail.com>
2019-06-25 19:22:19 -0400

    Updates

---
+++ precache-manifest.38fc4186fec5dd2d6b6d989ca880467a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5964efc1520f96d6936f98c8981eb87e",
+    "url": "/chattybox/index.html"
+  },
+  {
+    "revision": "7cc97cc8103d9aba51b2",
+    "url": "/chattybox/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b39072a66c336c7e4447",
+    "url": "/chattybox/static/js/2.988c469d.chunk.js"
+  },
+  {
+    "revision": "7cc97cc8103d9aba51b2",
+    "url": "/chattybox/static/js/main.dbd9b211.chunk.js"
+  },
+  {
+    "revision": "c6a746d3712821500629",
+    "url": "/chattybox/static/js/runtime~main.cd5edbdd.js"
+  }
+]);
